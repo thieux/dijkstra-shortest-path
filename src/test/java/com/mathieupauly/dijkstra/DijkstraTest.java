ce37cc68dac3f777f51bd0d3804f6163b1b53b04
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-07-27 21:30:43 +0200

    Initialise Maven project.

---
+++ src/test/java/com/mathieupauly/dijkstra/DijkstraTest.java
@@ -0,0 +1,12 @@
+package com.mathieupauly.dijkstra;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DijkstraTest {
+    @Test
+    public void canary() {
+        assertThat(0).isEqualTo(1);
+    }
+}
